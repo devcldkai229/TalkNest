c1d7cd4239b27cc4aa5f13493634f6c5d1584733
devcldkai229 <kag40222@gmail.com>
2025-05-31 08:34:19 +0700

    Implementing JwtService Dependency

--- backend/src/main/java/com/backend/TalkNestResourceServer/service/JwtService.java
+++ backend/src/main/java/com/backend/TalkNestResourceServer/service/JwtService.java
@@ -1,14 +1,19 @@
 package com.backend.TalkNestResourceServer.service;
 
-import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.jwt.JWTClaimsSet;
 import org.springframework.security.core.userdetails.User;
+
+import java.util.Map;
 
 public interface JwtService {
 
-    String generateToken(User user);
+    String generateToken(String subject, Map<String, Object> claims);
 
-    SignedJWT verifyToken(String token);
 
-    String buildScope(User user);
+    JWTClaimsSet verifyToken(String token);
+
+    String  extractUsername(User user);
+
+    Object extractClaim(String token, String claimName);
 
 }
--- backend/src/main/java/com/backend/TalkNestResourceServer/service/impl/JwtServiceImpl.java
+++ backend/src/main/java/com/backend/TalkNestResourceServer/service/impl/JwtServiceImpl.java
@@ -1,28 +1,54 @@
 package com.backend.TalkNestResourceServer.service.impl;
 
 
+import com.backend.TalkNestResourceServer.domain.helpers.JwtProps;
+import com.backend.TalkNestResourceServer.repository.UserRepository;
 import com.backend.TalkNestResourceServer.service.JwtService;
-import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class JwtServiceImpl implements JwtService {
 
+    private final JwtProps jwtProps;
+
+    private final JWSSigner signer;
+
+    private final JWSVerifier verifier;
+
+    public JwtServiceImpl(JwtProps jwtProperties) throws JOSEException {
+        this.jwtProps = jwtProperties;
+        byte[] secretBytes = jwtProperties.getSecret().getBytes();
+        this.signer = new MACSigner(secretBytes);
+        this.verifier = new MACVerifier(secretBytes);
+    }
 
 
     @Override
-    public String generateToken(User user) {
+    public String generateToken(String subject, Map<String, Object> claims) {
         return "";
     }
 
     @Override
-    public SignedJWT verifyToken(String token) {
+    public JWTClaimsSet verifyToken(String token) {
         return null;
     }
 
     @Override
-    public String buildScope(User user) {
+    public String extractUsername(User user) {
         return "";
+    }
+
+    @Override
+    public Object extractClaim(String token, String claimName) {
+        return null;
     }
 }
